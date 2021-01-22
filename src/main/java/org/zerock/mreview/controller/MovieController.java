515719ed5c8c333661c80a2350d67ca882555ea8
yonghoon-kim-nhn <yonghoon.kim@nhn.com>
2021-01-22 12:52:49 +0900

    chapter 9.2 완료 및 오류 수정

--- src/main/java/org/zerock/mreview/controller/MovieController.java
+++ src/main/java/org/zerock/mreview/controller/MovieController.java
@@ -4,11 +4,14 @@ package org.zerock.mreview.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.dto.PageRequestDTO;
+import org.zerock.mreview.dto.PageResultDTO;
 import org.zerock.mreview.service.MovieService;
 
 @Controller
@@ -29,5 +32,11 @@ public class MovieController {
         Long mno = movieService.register(movieDTO);
         redirectAttributes.addFlashAttribute("msg", mno);
         return "redirect:/movie/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info(("pageRequestDTO: " + pageRequestDTO));
+        model.addAttribute("result", movieService.getList(pageRequestDTO));
     }
 }
